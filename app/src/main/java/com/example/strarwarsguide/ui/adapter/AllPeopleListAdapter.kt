4f6c750d26942dd797d0294fc9f531b17026cc3c
juan gonzalo flores huanqui <jgonzalofh2898@gmail.com>
2021-02-11 10:12:32 -0500

    create adapter por recyclerview of allPeople

---
+++ app/src/main/java/com/example/strarwarsguide/ui/adapter/AllPeopleListAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.strarwarsguide.ui.adapter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.strarwarsguide.R
+import com.example.swapi.AllPeoplePaginatedQuery
+
+class AllPeopleListAdapter(
+    context: Context,
+    peopleList: MutableList<AllPeoplePaginatedQuery.Person>
+) : RecyclerView.Adapter<AllPeopleListAdapter.PersonViewHolder>() {
+    class PersonViewHolder(itemView: View, adapter: AllPeopleListAdapter) :
+        RecyclerView.ViewHolder(itemView) {
+        private val mAdapter = adapter
+        val personName: TextView = itemView.findViewById(R.id.personName)
+        val personFooter: TextView = itemView.findViewById(R.id.personFooter)
+
+        init {
+            itemView.setOnClickListener {
+                mAdapter.onItemClick(layoutPosition)
+            }
+        }
+    }
+
+    private val mContext = context
+    private val mPeopleList = peopleList
+    private val mInflater = LayoutInflater.from(context)
+
+    private var mOnItemClickListener: ((AllPeoplePaginatedQuery.Person) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonViewHolder {
+        val mItemView = mInflater.inflate(R.layout.item_person, parent, false)
+        return PersonViewHolder(mItemView, this)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: PersonViewHolder, position: Int) {
+        val person = mPeopleList[position]
+        holder.personName.text = person.name
+
+        val species = person.species?.name ?: "Human"
+        val homeWorld = person.homeworld?.name ?: "Earth"
+        holder.personFooter.text = "$species from $homeWorld"
+    }
+
+    override fun getItemCount(): Int {
+        return mPeopleList.size
+    }
+
+    fun onItemClickListener(listener: (AllPeoplePaginatedQuery.Person) -> Unit) {
+        mOnItemClickListener = listener
+    }
+
+    private fun onItemClick(position: Int) {
+        mOnItemClickListener?.invoke(mPeopleList[position])
+    }
+}
